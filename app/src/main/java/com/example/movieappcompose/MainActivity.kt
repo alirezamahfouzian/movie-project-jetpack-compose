e4d70a96210f034736d4534e9a17d3c86de6bdd1
Alireza Mahfouzian <alirezamahfouzian777@gmail.com>
2022-12-08 12:37:06 +0330

    init commit

---
+++ app/src/main/java/com/example/movieappcompose/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.movieappcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.movieappcompose.ui.MovieAppComposeApp
+import com.example.movieappcompose.ui.theme.MovieAppComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MovieAppComposeApp()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/movieappcompose/ui/MovieAppComposeApp.kt
@@ -0,0 +1,19 @@
+package com.example.movieappcompose.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.movieappcompose.ui.theme.MovieAppComposeTheme
+
+@Composable
+fun MovieAppComposeApp() {
+    MovieAppComposeTheme {
+    }
+}
+
